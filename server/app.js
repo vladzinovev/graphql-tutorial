b680d76352b3ac022e249182befa398e3803d04b
Vladislav <vladozdeveloper@gmail.com>
2022-10-14 23:54:13 +0300

    Connected DB on the mongodb

--- server/app.js
+++ server/app.js
@@ -4,6 +4,9 @@ const {graphqlHTTP} = require('express-graphql');
 
 const schema = require('../schema/schema');
 
+const mongoose=require('mongoose');
+mongoose.connect('mongodb+srv://admin:hh9OjLA6P1mIGiq6@graphql-tutorial.hhpocja.mongodb.net/test',{useMongoClient:true});
+
 const app = express();
 //указываем  порт
 const PORT = 3000;
@@ -12,7 +15,17 @@ app.use('/graphql', graphqlHTTP({
   schema,
   graphiql:true,
 })); 
+
+//подключение к бд
+const dbConnection=mongoose.connection;
+dbConnection.on('error',err=>console.log(`Connection error: ${err}`));
+dbConnection.once('open',()=>console.log('Connected to DB!'));
+
 //запускаем прослушивание нашего порта
 app.listen(PORT, err => {
   err ? console.log(err) : console.log('Server started!');
 });
+
+
+
+
